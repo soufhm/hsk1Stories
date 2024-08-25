144f475a36962cc9c239fbef7bc5945104b65ed8
soufhm <SoufyaneHamimed@my.uopeople.edu>
2024-08-25 09:44:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const audioPlayer = document.getElementById('audio-player');
 let currentStoryIndex = 0;
 let stories = [
     {
-        chinese: "这是一个美好的一天，我在北京的学校里学习汉语。今天的天气很好，没有下雨。中午，我和朋友****一起去商店买了些东西。他买了一本书，我买了一点儿水果和茶。下午，我们坐在学校的桌子旁边，一边喝****茶一边聊天。",
+        chinese: "故事一： 上 不 个 书 买 了 些 人 他 会 住 你 做 写 冷 几 去 叫 号 吃 吗 听 呢 和 哪 喂 喝 回 在 坐 块 多 大 太 她 好 字 家 小 少 岁 年 开 很 想 我 是 月 有 本 来 水 点 热 爱 狗 猫 的 看 能 茶 菜 说 请 读 谁 这 那 都 里 钱 一 七 三 下 九 二 五 八 六 十 四 上午 下午 下雨 东西 中午 中国 什么 今天 儿子 先生 再见 分钟 前面 北京 医生 医院 同学 名字",
         arabic: "قصة 1:  كان هناك رجل اسمه لي. كان لي يعيش في مدينة صغيرة. كان لي لديه صديق اسمه وانغ. كان وانغ يحب أن يلعب كرة القدم. كان لي يحب أن يقرأ الكتب. كان لي ووانغ صديقين مقربين.", 
         english: "Story 1: There was a man named Li. Li lived in a small city. Li had a friend named Wang. Wang loved to play football. Li loved to read books. Li and Wang were good friends.",
         audioSrc: "audio/story1.mp3" 
@@ -37,6 +37,20 @@ function updateStory() {
     updateButtonStates();
     currentStory.classList.remove('ar');
     audioPlayer.src = stories[currentStoryIndex].audioSrc; 
+    audioPlayer.setAttribute("controls", "controls");
+
+    // Prevent downloading
+    audioPlayer.oncontextmenu = function(event) {
+        event.preventDefault();
+    };
+    audioPlayer.onmousedown = function(event) {
+        if (event.button === 2) {
+            event.preventDefault();
+        }
+    };
+    audioPlayer.addEventListener('click', function() {
+        this.play();
+    });
 }
 
 function updateButtonStates() {
